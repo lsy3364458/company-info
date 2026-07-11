032753d0717c1bffde89a42300775b69149e313a
Xinyou Dev <dev@xinyou.com>
2026-07-11 20:59:51 +0800

    Hardcode base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
+  base: '/company-info/',
   plugins: [react()],
   server: {
     port: 3000,
